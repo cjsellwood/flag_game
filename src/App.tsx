eec7a008eb8661b6d571a88f6865523eb28ab2a2
cjsellwood <c.sellwood22@gmail.com>
2022-10-25 16:47:06 +1100

    Game can be played continously, but only with random flags

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,38 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Trie } from "../scripts/Trie";
 import countries from "./countries.json";
 import countryTrie from "./countryTrie.json";
+import Guess from "./components/Guess";
 
 const trie = new Trie(countryTrie.root);
 
 function App() {
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-  const [countryInput, setCountryInput] = useState("");
-  const [showSuggestions, setShowSuggestions] = useState(false);
-
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setCountryInput(e.target.value);
-    if (e.target.value === "") {
-      setSuggestions([]);
-      return;
+  const [currentCountry, setCurrentCountry] = useState(
+    Math.floor(Math.random() * countries.length)
+  );
+  const [correct, setCorrect] = useState(0);
+  const [wrong, setWrong] = useState(0);
+  const guessFlag = (isCorrect: boolean) => {
+    if (isCorrect) {
+      setCorrect((correct) => correct + 1);
+    } else {
+      setWrong((wrong) => wrong + 1);
     }
-    setShowSuggestions(true);
-    const possibilities = trie.findAllWords(e.target.value.toLowerCase());
-    setSuggestions(possibilities);
+    setCurrentCountry(Math.floor(Math.random() * countries.length));
   };
-
-  const selectCountry = (suggestion: string) => {
-    setCountryInput(suggestion);
-    setShowSuggestions(false);
-  };
-
-  const submitAnswer = () => {};
   return (
     <div className="App">
-      <div
-        className="flag-container"
-        style={{
-          backgroundImage: `url(${
-            countries[Math.floor(Math.random() * 250)].flag
-          })`,
-        }}
-      ></div>
-      <div className="input-container">
-        <input
-          className="country-input"
-          onChange={handleInput}
-          value={countryInput}
-        />
-        {showSuggestions && (
-          <ul className="suggestion-list">
-            {suggestions.map((suggestion) => {
-              return (
-                <li key={suggestion} onClick={() => selectCountry(suggestion)}>
-                  {suggestion}
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      {countryInput !== "" && !showSuggestions && (
-        <button className="submit-button" onClick={submitAnswer}>
-          Submit
-        </button>
-      )}
+      <Guess
+        trie={trie}
+        country={countries[currentCountry]}
+        guessFlag={guessFlag}
+        key={countries[currentCountry].ISOCode}
+      />
+      <footer className="footer">
+        <div>Correct: {correct}</div>
+        <div>Wrong: {wrong}</div>
+      </footer>
     </div>
   );
 }
